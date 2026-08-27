fc675e8e997a3ad6c904af98ca08d6406759b137
Prince <princemmolotsi@gmail.com>
2026-08-27 03:08:09 +0200

    wrong placement of *

--- backend/server.js
+++ backend/server.js
@@ -44,7 +44,7 @@ app.use("/api/orders", orderRoutes);
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
-  app.get("/*{splat}", (req, res) => {
+  app.get("/{*splat}", (req, res) => {
     res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
   });
 }
